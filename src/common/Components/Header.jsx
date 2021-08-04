4cbcf53964c02f3da510e6a2e9c9f3a8b6dc453b
Adão Benites <adaobjr@gmail.com>
2021-08-04 15:05:44 -0400

    fix: :art: update classNames in Header

--- src/common/Components/Header.jsx
+++ src/common/Components/Header.jsx
@@ -8,22 +8,22 @@ export default function Header() {
   return (
     <>
       {/* <!--========== HEADER ==========--> */}
-      <header className="l-header" id="header">
-        <nav className="nav bd-container">
-          <a href className="nav__logo">Christmas Gift</a>
+      <header className="header" id="header">
+        <nav className="nav bdContainer">
+          <a href className="navLogo">Christmas Gift</a>
 
-          <div className="nav__menu" id="nav-menu">
-            <ul className="nav__list">
-              <li className="nav__item"><a href="#home" className="nav__link active-link">Home</a></li>
-              <li className="nav__item"><Link to={Favoritos} className="nav__link">Favoritos</Link></li>
-              <li className="nav__item"><a href="#decoration" className="nav__link">Decorations</a></li>
-              <li className="nav__item"><a href="#accessory" className="nav__link">Accessory</a></li>
+          <div className="navMenu" id="nav-menu">
+            <ul className="navList">
+              <li className="navItem"><a href="#home" className="navLink activeLink">Home</a></li>
+              <li className="navItem"><Link to={Favoritos} className="navLink">Favoritos</Link></li>
+              <li className="navItem"><a href="#decoration" className="navLink">Decorations</a></li>
+              <li className="navItem"><a href="#accessory" className="navLink">Accessory</a></li>
 
-              <li className="change-theme" id="theme-button"><BiToggleLeft /></li>
+              <li className="changeTheme" id="theme-button"><BiToggleLeft /></li>
             </ul>
           </div>
 
-          <div className="nav__toggle" id="nav-toggle">
+          <div className="navToggle" id="nav-toggle">
             <BiToggleRight />
           </div>
         </nav>
