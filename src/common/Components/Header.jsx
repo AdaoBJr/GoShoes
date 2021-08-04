6b4c8d58e7ec585808f33073347b45d7e343a29e
Adão Benites <adaobjr@gmail.com>
2021-08-04 08:32:31 -0400

    feat: :sparkles: installed React-Router / Router-dom

--- src/common/Components/Header.jsx
+++ src/common/Components/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { BiToggleLeft, BiToggleRight } from 'react-icons/bi';
+import Favoritos from '../Pages/Favoritos';
 
 export default function Header() {
   return (
@@ -12,7 +15,7 @@ export default function Header() {
           <div className="nav__menu" id="nav-menu">
             <ul className="nav__list">
               <li className="nav__item"><a href="#home" className="nav__link active-link">Home</a></li>
-              <li className="nav__item"><a href="#share" className="nav__link">Share</a></li>
+              <li className="nav__item"><Link to={Favoritos} className="nav__link">Favoritos</Link></li>
               <li className="nav__item"><a href="#decoration" className="nav__link">Decorations</a></li>
               <li className="nav__item"><a href="#accessory" className="nav__link">Accessory</a></li>
 
