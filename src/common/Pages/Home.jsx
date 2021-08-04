1351d4d255e94760816efe1fabd6c0e0935e69ce
Adão Benites <adaobjr@gmail.com>
2021-08-04 11:21:33 -0400

    style: :sparkles: added images in Home Page

--- src/common/Pages/Home.jsx
+++ src/common/Pages/Home.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { SiFacebook, SiTwitter, SiInstagram } from 'react-icons/si';
+
 import Header from '../Components/Header';
 import ScrollTop from '../Components/ScrollTop';
+import Shoes from '../Components/Shoes';
+import img1 from '../../images/img1.png';
+import img2 from '../../images/img2.png';
+import img3 from '../../images/img3.png';
+import img4 from '../../images/img4.png';
+import img5 from '../../images/img5.png';
+import img6 from '../../images/img6.png';
 
 export default function Home() {
   return (
@@ -11,146 +20,40 @@ export default function Home() {
       <Header />
 
       <main className="l-main">
-        {/* <!--========== HOME ==========--> */}
+        {/* <!--========== HOME ==========--> INSERIR AQUI 3D */}
         <section className="home" id="home">
-          <div className="home__container bd-container bd-grid">
+          <div className="home__container bd-grid">
             <div className="home__img">
-              <img src="assets/img/home.png" alt="" />
+              <img src={img1} alt="img-1" data-speed="-2" className="move" />
+              <img src={img2} alt="img-2" data-speed="2" className="move" />
+              <img src={img3} alt="img-3" data-speed="2" className="move" />
+              <img src={img4} alt="img-4" data-speed="-2" className="move" />
+              <img src={img5} alt="img-5" data-speed="-2" className="move" />
+              <img src={img6} alt="img-6" data-speed="2" className="move" />
             </div>
 
             <div className="home__data">
-              <h1 className="home__title">Send A Gift For Christmas</h1>
-              <p className="home__description">In these end of the year holidays send a gift to your loved one and share the happiness at Christmas.</p>
-              <a href="#" className="button">Get Started</a>
-            </div>
-          </div>
-        </section>
-
-        {/* <!--========== SHARE ==========--> */}
-        <section className="share section bd-container" id="share">
-          <div className="share__container bd-grid">
-            <div className="share__data">
-              <h2 className="section-title-center">
-                Sharing Is The Best Gift
+              <h1 className="home__title">
+                Go
+                <br />
+                {' '}
+                Shoes
+              </h1>
+              <p className="home__description">
+                Let`s shop?
                 {' '}
                 <br />
                 {' '}
-                You Can Give
-              </h2>
-              <p className="share__description">Sharing these holidays is the best gift you can give, give a present or share your love with the people you love the most and celebrate with great happiness.</p>
-              <a href className="button">Send a Gift</a>
-            </div>
-
-            <div className="share__img">
-              <img src="assets/img/shared.png" alt="" />
+                Let`s GoShoes.
+              </p>
+              <a href className="home__button">Get Started</a>
             </div>
           </div>
         </section>
 
-        {/* <!--========== DECORATION ==========--> */}
-        <section className="decoration section bd-container" id="decoration">
-          <h2 className="section-title">
-            Give Christmas Decorations
-            {' '}
-            <br />
-            {' '}
-            For Your Home
-          </h2>
-          <div className="decoration__container bd-grid">
-            <div className="decoration__data">
-              <img src="assets/img/decoration1.png" alt="" className="decoration__img" />
-              <h3 className="decoration__title">Christmas Bells</h3>
-              <a href className="button button-link">Go Shopping</a>
-            </div>
-
-            <div className="decoration__data">
-              <img src="assets/img/decoration2.png" alt="" className="decoration__img" />
-              <h3 className="decoration__title">Christmas Candies</h3>
-              <a href className="button button-link">Go Shopping</a>
-            </div>
-
-            <div className="decoration__data">
-              <img src="assets/img/decoration3.png" alt="" className="decoration__img" />
-              <h3 className="decoration__title">Christmas Trees</h3>
-              <a href className="button button-link">Go Shopping</a>
-            </div>
-          </div>
-        </section>
-
-        {/* <!--========== ACCESSORIES ==========--> */}
-        <section className="accessory section bd-container" id="accessory">
-          <h2 className="section-title">
-            New Christmas
-            {' '}
-            <br />
-            {' '}
-            Accessories
-          </h2>
-
-          <div className="accessory__container bd-grid">
-            <div className="accessory__content">
-              <img src="assets/img/accessory1.png" alt="" className="accessory__img" />
-              <h3 className="accessory__title">Snow Globe</h3>
-              <span className="accessory__category">Accessory</span>
-              <span className="accessory__preci">$9.45</span>
-              <a href className="button accessory__button"><i className="bx bx-heart" /></a>
-            </div>
-
-            <div className="accessory__content">
-              <img src="assets/img/accessory2.png" alt="" className="accessory__img" />
-              <h3 className="accessory__title">Candy</h3>
-              <span className="accessory__category">Accessory</span>
-              <span className="accessory__preci">$2.52</span>
-              <a href className="button accessory__button"><i className="bx bx-heart" /></a>
-            </div>
-
-            <div className="accessory__content">
-              <img src="assets/img/accessory3.png" alt="" className="accessory__img" />
-              <h3 className="accessory__title">Angel</h3>
-              <span className="accessory__category">Accessory</span>
-              <span className="accessory__preci">$13.15</span>
-              <a href className="button accessory__button"><i className="bx bx-heart" /></a>
-            </div>
-
-            <div className="accessory__content">
-              <img src="assets/img/accessory4.png" alt="" className="accessory__img" />
-              <h3 className="accessory__title">Sphere</h3>
-              <span className="accessory__category">Accessory</span>
-              <span className="accessory__preci">$4.25</span>
-              <a href="#" className="button accessory__button"><i className="bx bx-heart" /></a>
-            </div>
-
-            <div className="accessory__content">
-              <img src="assets/img/accessory5.png" alt="" className="accessory__img" />
-              <h3 className="accessory__title">Surprise gift</h3>
-              <span className="accessory__category">Accessory</span>
-              <span className="accessory__preci">$7.99</span>
-              <a href className="button accessory__button"><i className="bx bx-heart" /></a>
-            </div>
-          </div>
-        </section>
-
-        {/* <!--========== SEND GIFT ==========--> */}
-        <section className="send section">
-          <div className="send__container bd-container bd-grid">
-            <div className="send__content">
-              <h2 className="section-title-center send__title">Send Gift Now</h2>
-              <p className="send__description">Start giving away before the holidays are over. Write the home address of the person who will send the gift.</p>
-              <form action="">
-                <div className="send__direction">
-                  <input type="text" placeholder="House address" className="send__input" />
-                  <a href className="button">Send</a>
-                </div>
-              </form>
-            </div>
-
-            <div className="send__img">
-              <img src="assets/img/send.png" alt="" />
-            </div>
-          </div>
-        </section>
+        {/* <!--========== COLEÇÕES ==========--> INSERIR AQUI PRODUTOS / COMPONETIZAR */}
+        <Shoes />
       </main>
-
       {/* <!--========== FOOTER ==========--> */}
       <footer className="footer section">
         <div className="footer__container bd-container bd-grid">
@@ -187,9 +90,9 @@ export default function Home() {
 
           <div className="footer__content">
             <h3 className="footer__title">Social</h3>
-            <a href className="footer__social"><i className="bx bxl-facebook-circle " /></a>
-            <a href className="footer__social"><i className="bx bxl-twitter" /></a>
-            <a href className="footer__social"><i className="bx bxl-instagram-alt" /></a>
+            <SiFacebook className="footer__social" />
+            <SiTwitter className="footer__social" />
+            <SiInstagram className="footer__social" />
           </div>
         </div>
 
