ed3575bbfe5e3cf6cdd8fe1b5051195dbf46a74d
Adão Benites <adaobjr@gmail.com>
2021-08-05 18:10:42 -0400

    feat: :lipstick: added heart icon / adjust background home

--- src/common/Components/Shoes.jsx
+++ src/common/Components/Shoes.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { /* FaHeart, */ FaRegHeart } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import img6 from '../../images/img6.png';
 
 export default function Shoes() {
+  const [fullHeart, setHeart] = useState(false);
   return (
     <>
       {/* <!--========== COLEÇÕES ==========--> */}
@@ -10,45 +12,79 @@ export default function Shoes() {
           Calçados e Acessórios
         </h2>
 
+        <h2 className="collectionTitle">
+          Novas Coleções
+        </h2>
+
         <div className="shoesContainer bdGrid">
           <div className="shoesContent">
-            <img src="assets/img/shoes1.png" alt="" className="shoesImg" />
+            <img src={img6} alt="" className="shoesImg" />
             <h3 className="shoesTitle">Snow Globe</h3>
             <span className="shoesCategory">shoes</span>
             <span className="shoesPreci">$9.45</span>
-            <FaRegHeart className="button shoesButton" />
+            <div
+              aria-hidden
+              className="button shoesButton"
+              onClick={() => setHeart(!fullHeart)}
+            >
+              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
+            </div>
           </div>
 
           <div className="shoesContent">
-            <img src="assets/img/shoes2.png" alt="" className="shoesImg" />
+            <img src={img6} alt="" className="shoesImg" />
             <h3 className="shoesTitle">Candy</h3>
             <span className="shoesCategory">shoes</span>
             <span className="shoesPreci">$2.52</span>
-            <FaRegHeart className="button shoesButton" />
+            <div
+              aria-hidden
+              className="button shoesButton"
+              onClick={() => setHeart(!fullHeart)}
+            >
+              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
+            </div>
           </div>
 
           <div className="shoesContent">
-            <img src="assets/img/shoes3.png" alt="" className="shoesImg" />
+            <img src={img6} alt="" className="shoesImg" />
             <h3 className="shoesTitle">Angel</h3>
             <span className="shoesCategory">shoes</span>
             <span className="shoesPreci">$13.15</span>
-            <FaRegHeart className="button shoesButton" />
+            <div
+              aria-hidden
+              className="button shoesButton"
+              onClick={() => setHeart(!fullHeart)}
+            >
+              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
+            </div>
           </div>
 
           <div className="shoesContent">
-            <img src="assets/img/shoes4.png" alt="" className="shoesImg" />
+            <img src={img6} alt="" className="shoesImg" />
             <h3 className="shoesTitle">Sphere</h3>
             <span className="shoesCategory">shoes</span>
             <span className="shoesPreci">$4.25</span>
-            <FaRegHeart className="button shoesButton" />
+            <div
+              aria-hidden
+              className="button shoesButton"
+              onClick={() => setHeart(!fullHeart)}
+            >
+              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
+            </div>
           </div>
 
           <div className="shoesContent">
-            <img src="assets/img/shoes5.png" alt="" className="shoesImg" />
+            <img src={img6} alt="" className="shoesImg" />
             <h3 className="shoesTitle">Surprise gift</h3>
             <span className="shoesCategory">shoes</span>
             <span className="shoesPreci">$7.99</span>
-            <FaRegHeart className="button shoesButton" />
+            <div
+              aria-hidden
+              className="button shoesButton"
+              onClick={() => setHeart(!fullHeart)}
+            >
+              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
+            </div>
           </div>
         </div>
       </section>
