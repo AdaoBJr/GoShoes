0037f437157b8e1ee178c7c6acc0687e569fe8c5
Adão Benites <adaobjr@gmail.com>
2021-08-06 14:29:59 -0400

    feat: :sparkles: created ContextAPI

---
+++ src/context/store.js
@@ -0,0 +1,35 @@
+// store = Context API
+import { createContext } from 'react';
+
+// INITIAL STATES ----------------------------------------------------------------
+export const USER = {
+  email: '',
+  password: '',
+};
+
+export const PRODUCTS = {
+  fetchOn: true,
+  loading: undefined,
+  done: undefined,
+  products: [],
+};
+
+export const CART = {
+  cart: [],
+};
+
+// COMBINE -----------------------------------------------------------------------
+
+export const INITIAL_STATE = { ...USER, ...PRODUCTS, ...CART };
+
+const store = createContext(INITIAL_STATE);
+
+export default store;
+
+// ACTIONS -----------------------------------------------------------------------
+
+// USER
+export const ADD_PRODUCTS = 'ADD_PRODUCTS'; // ACTION -> ADD_PRODUCTS
+export const addProducts = (products) => ({ // ACTION-CREATOR -> ADD_PRODUCTS
+  type: ADD_PRODUCTS, payload: { products },
+});
