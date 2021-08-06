a0b080fd6fc688ef2f545cefeb7c04bcec921884
Adão Benites <adaobjr@gmail.com>
2021-08-06 14:34:42 -0400

    refactor: :zap: created function renderProducts in Shoes Component

--- src/common/Components/Shoes.jsx
+++ src/common/Components/Shoes.jsx
@@ -6,108 +6,62 @@ import img6 from '../../images/img6.png';
 
 export default function Shoes() {
   const [fullHeart, setHeart] = useState(false);
+
+  const renderProducts = () => (
+    <section className="shoes section bdContainer" id="shoes">
+      <h2 className="sectionTitle">
+        Calçados e Acessórios
+      </h2>
+
+      <h2 className="collectionTitle">
+        Novas Coleções
+      </h2>
+
+      <div className="shoesContainer bdGrid">
+        <div className="shoesContent">
+          <img src={img6} alt="" className="shoesImg" />
+          <h3 className="shoesTitle">Candy</h3>
+          <span className="shoesCategory">shoes</span>
+          <span className="shoesPreci">$2.52</span>
+          <div className="addRemoveButtons">
+            <div
+              aria-hidden
+              className="removeButton"
+            >
+              <FaMinus />
+            </div>
+            <div className="cartItems">
+              <FaShoppingCart />
+              <div className="numberItems">3</div>
+            </div>
+            <div
+              aria-hidden
+              className="addButton"
+            >
+              <FaPlus />
+            </div>
+          </div>
+          <div
+            aria-hidden
+            className="button favoritedButton"
+            onClick={() => setHeart(!fullHeart)}
+          >
+            {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+
+  // ----------------------------------------------------------------------------------------------
+  // CICLOS DE VIDA
+
+  // ----------------------------------------------------------------------------------------------
+
   return (
     <>
       {/* <!--========== COLEÇÕES ==========--> */}
-      <section className="shoes section bdContainer" id="shoes">
-        <h2 className="sectionTitle">
-          Calçados e Acessórios
-        </h2>
-
-        <h2 className="collectionTitle">
-          Novas Coleções
-        </h2>
-
-        <div className="shoesContainer bdGrid">
-          <div className="shoesContent">
-            <img src={img6} alt="" className="shoesImg" />
-            <h3 className="shoesTitle">Snow Globe</h3>
-            <span className="shoesCategory">shoes</span>
-            <span className="shoesPreci">$9.45</span>
-            <div
-              aria-hidden
-              className="button favoritedButton"
-              onClick={() => setHeart(!fullHeart)}
-            >
-              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
-            </div>
-          </div>
-
-          <div className="shoesContent">
-            <img src={img6} alt="" className="shoesImg" />
-            <h3 className="shoesTitle">Candy</h3>
-            <span className="shoesCategory">shoes</span>
-            <span className="shoesPreci">$2.52</span>
-            <div className="addRemoveButtons">
-              <div
-                aria-hidden
-                className="removeButton"
-              >
-                <FaMinus />
-              </div>
-              <div className="cartItems">
-                <FaShoppingCart />
-                <div className="numberItems">3</div>
-              </div>
-              <div
-                aria-hidden
-                className="addButton"
-              >
-                <FaPlus />
-              </div>
-            </div>
-            <div
-              aria-hidden
-              className="button favoritedButton"
-              onClick={() => setHeart(!fullHeart)}
-            >
-              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
-            </div>
-          </div>
-
-          <div className="shoesContent">
-            <img src={img6} alt="" className="shoesImg" />
-            <h3 className="shoesTitle">Angel</h3>
-            <span className="shoesCategory">shoes</span>
-            <span className="shoesPreci">$13.15</span>
-            <div
-              aria-hidden
-              className="button favoritedButton"
-              onClick={() => setHeart(!fullHeart)}
-            >
-              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
-            </div>
-          </div>
-
-          <div className="shoesContent">
-            <img src={img6} alt="" className="shoesImg" />
-            <h3 className="shoesTitle">Sphere</h3>
-            <span className="shoesCategory">shoes</span>
-            <span className="shoesPreci">$4.25</span>
-            <div
-              aria-hidden
-              className="button favoritedButton"
-              onClick={() => setHeart(!fullHeart)}
-            >
-              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
-            </div>
-          </div>
-
-          <div className="shoesContent">
-            <img src={img6} alt="" className="shoesImg" />
-            <h3 className="shoesTitle">Surprise gift</h3>
-            <span className="shoesCategory">shoes</span>
-            <span className="shoesPreci">$7.99</span>
-            <div
-              aria-hidden
-              className="button favoritedButton"
-              onClick={() => setHeart(!fullHeart)}
-            >
-              {(fullHeart) ? <FaHeart /> : <FaRegHeart /> }
-            </div>
-          </div>
-        </div>
-      </section>
+      {renderProducts()}
     </>
   );
 }
